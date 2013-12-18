bea00558b5e1d931f625f6f087c94d7625707aa7
bittercup <spatterson8@gmail.com>
2013-12-18 04:00:37 -0500

    generalized search vars & enabled user defined search with input and query method

--- ani.rb
+++ ani.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
+
 # Method to output queried anime to screen
 def to_screen(id, title, desc)
 	# Formatting output  #
@@ -18,24 +19,26 @@ def to_screen(id, title, desc)
 	puts "\n#{desc}\n"
 end
 
-# Method to store matches to robot anime
-def robot_query(id, title, desc, robot_db)
-	## >> Make this into a search/genre selection method
-	# # Robots check
-	if(desc.match(/robot/))
+# Method to store matches
+def search_query(id, title, desc, search_db, search_term)
+	# If anything in description matches term, record to file
+	if(desc.match(/#{search_term}/))
 		# Open a file and record the matching entry
-		File.open("anime_robots.txt", "a") do
+		File.open("anime_matches.txt", "a") do
 			|file| file.write("\n\n[ID\# #{id}] Title: #{title}\n----------\nDescription:\n----------\n#{desc}\n")
 		end
-		robot_db.push(desc)
+		# Store the description in search_db
+		search_db.push(desc)
+		# Output found results to screen
 		to_screen(id, title, desc)
 	end
 end
 
 
+
 # Starting values
 id = 0
-robot_db = []
+search_db = []
 ani_db = []
 
 ### User input section ###
@@ -45,6 +48,9 @@ entry_num = gets.chomp
 # Get user command for anime ID to begin at
 print "Which anime ID would you like to begin at?\n>> "
 id = gets.chomp.to_i
+# Get user search term
+print "What term do you want to search for?\n>> "
+search_term = gets.chomp.to_s
 
 # Loading message
 print "Loading now, please wait...\n------------\n"
@@ -61,8 +67,8 @@ print "Loading now, please wait...\n------------\n"
 		title = data.at_css("#series_right h1").text.strip
 		desc = data.at_css("#series_des").text.strip
 
-		# Call robot matching method
-		robot_query(id, title, desc, robot_db)
+		# Call anime matching method
+		search_query(id, title, desc, search_db, search_term)
 		# Push total titles and id into ani_db
 		ani_db.push("ID\# (#{id}) " + title)
 
@@ -81,4 +87,4 @@ end
 # Print out report
 puts "\nTotal list queried: \n------------"
 ani_db.each { |title| puts title}
-puts "------------\nNumber of robot anime's found: " + robot_db.length.to_s + " out of " + ani_db.length.to_s
+puts "------------\nNumber of #{search_term} anime's found: " + search_db.length.to_s + " out of " + ani_db.length.to_s
